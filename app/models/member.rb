6c01b12b8e1b19120cac003d8cbb7e418d0d7a93
High-how <1215ikkou@gmail.com>
2020-03-29 16:18:44 +0900

    form_for

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ApplicationRecord
   has_many :user_members
-  has_many :users, though: :user_members
+  has_many :users, through: :user_members
   validates :name, presence: true, uniqueness: true
 end
